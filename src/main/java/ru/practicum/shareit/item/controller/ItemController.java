e7824855c455bfacca9562f6acf9500eca99e926
Aleh Kavalkou <osk08880@yandex.ru>
2025-11-18 22:33:00 +0300

    Добавили userId в findById и пагинацию в findAllByOwner.

--- src/main/java/ru/practicum/shareit/item/controller/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/controller/ItemController.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.item.dto.CommentDto;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.service.ItemService;
 
@@ -36,13 +37,16 @@ public class ItemController {
     }
 
     @GetMapping("/{itemId}")
-    public ItemDto findById(@PathVariable Long itemId) {
-        log.info("Запрос GET /items/{} - получение вещи", itemId);
-        return itemService.findById(itemId);
+    public ItemDto findById(@RequestHeader(USER_ID_HEADER) Long userId,
+                            @PathVariable Long itemId) {
+        log.info("Запрос GET /items/{} - получение вещи пользователем {}", itemId, userId);
+        return itemService.findById(userId, itemId);
     }
 
     @GetMapping
-    public List<ItemDto> findAllByOwner(@RequestHeader(USER_ID_HEADER) Long userId) {
+    public List<ItemDto> findAllByOwner(@RequestHeader(USER_ID_HEADER) Long userId,
+                                        @RequestParam(defaultValue = "0") int from,
+                                        @RequestParam(defaultValue = "10") int size) {
         log.info("Запрос GET /items - получение всех вещей пользователя {}", userId);
         return itemService.findAllByOwner(userId);
     }
@@ -51,5 +55,13 @@ public class ItemController {
     public List<ItemDto> search(@RequestParam String text) {
         log.info("Запрос GET /items/search?text={} - поиск вещей", text);
         return itemService.search(text);
+    }
+
+    @PostMapping("/{itemId}/comment")
+    public CommentDto addComment(@PathVariable Long itemId,
+                                 @Valid @RequestBody CommentDto commentDto,
+                                 @RequestHeader(USER_ID_HEADER) Long userId) {
+        log.info("Запрос POST /items/{}/comment - добавление отзыва пользователем {}", itemId, userId);
+        return itemService.addComment(commentDto, userId, itemId);
     }
 }
