c7b0bf001a15f5f6f3d9f37da1cde510999c1891
Aleh Kavalkou <osk08880@yandex.ru>
2025-12-07 02:21:17 +0300

    Fix

--- server/src/main/java/ru/practicum/shareit/ShareItServerApplication.java
+++ server/src/main/java/ru/practicum/shareit/ShareItServerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShareItServerApplication {
     public static void main(String[] args) {
+
         SpringApplication.run(ShareItServerApplication.class, args);
     }
 }
